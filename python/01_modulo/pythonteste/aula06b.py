55480ce9d469111efe3f755350678ea6dff37235
leonardoklestadtluz <klestadt@hotmail.com>
2024-02-17 10:30:20 -0300

    conclusão da Aula 9 - Manipulando Texto

---
+++ python/01_modulo/pythonteste/aula06b.py
@@ -0,0 +1,13 @@
+n = int(input('Digite um valor: '))
+print(type(n), n)
+
+n = float(input('Digite outro valor: '))
+print(type(n), n)
+
+n = bool(input('Mais um valor: '))
+print(type(n), n)
+
+n = str(input('O último valor, eu prometo: '))
+print(type(n), n)
+
+
---
+++ python/cursoEmVideo/exemplos/ex003.py
@@ -0,0 +1,4 @@
+numero01 = input('Digite o 1º número: ')
+numero02 = input('Digite o 2º número: ')
+soma = int(numero01) + int(numero02)
+print('A soma é:', soma)
