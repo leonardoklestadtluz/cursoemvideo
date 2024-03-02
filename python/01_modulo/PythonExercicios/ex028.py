a30803cf97b529eb062399eb0fc056252db956ec
Leonardo Klestadt Luz <klestadt@hotmail.com>
2024-03-02 09:50:54 -0300

    conclusão da aula Exercício 28 - Jogo da Adivinhação v.1.0

--- python/01_modulo/PythonExercicios/ex028.py
+++ python/01_modulo/PythonExercicios/ex028.py
@@ -5,15 +5,27 @@ descobrir qual foi o número escolhido pelo computador.
 O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
 """
-import random
+from random import randint
+from time import sleep
 
-numeroDigitado = print('Qual número estou pensando?')
+print('-=-' * 20)
+print('OLÁ! Digite um número entre 0 e 5!')
+print('-=-' * 20, '\n')
 
-numeroAleatorio = random.randint(0, 5)
+numeroAleatorio = randint(0, 5)
+numeroDigitado = int(input('Qual número estou pensando? '))
+print('PROCESSANDO...')
+sleep(3)
+mensagemErro = '#### ATENÇÃO, digite um número entre 0 e 5! #####'
 
+if numeroDigitado > 5:
+    print(mensagemErro)
+
+if numeroDigitado < 0:
+    print(mensagemErro)
 
 if numeroAleatorio == numeroDigitado:
-    print(f'PARABÉNS! Você digitou o número: {numeroDigitado} acertou.')
+    print(f'PARABÉNS! Você digitou o número: {numeroDigitado} ACERTOU!!!')
 else:
     print(f'ERROU! Número digitado por você: {numeroDigitado}.')
     print(f'Eu pensei no número {numeroAleatorio}.')
