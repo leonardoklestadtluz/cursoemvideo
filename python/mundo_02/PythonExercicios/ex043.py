629778ce91064be981f3f27ac739dd163b3e52eb
Leonardo Klestadt Luz <klestadt@hotmail.com>
2024-03-24 11:43:42 -0300

    exercício 43 concluído e 44 em andamento

--- python/mundo_02/PythonExercicios/ex043.py
+++ python/mundo_02/PythonExercicios/ex043.py
@@ -1,6 +1,7 @@
 """
 
-Desenvolva uma lógica que leia o peso e altura de uma pessoa, calcule seu IMC e mostre seu status, de acordo com a tabela abaixo:
+Desenvolva uma lógica que leia o peso e altura de uma pessoa,
+calcule seu IMC e mostre seu status, de acordo com a tabela abaixo:
 
 - Abaixo de 18.5: Abaixo do peso
 - Entre 18.5 e 25: Peso ideal
@@ -8,3 +9,29 @@ Desenvolva uma lógica que leia o peso e altura de uma pessoa, calcule seu IMC e
 - 30 até 40: Obesidade
 - Acima de 40: Obesidade mórbida
 """
+print('&=' * 12)
+print('ÍNDICE DE MASSA CORPORAL')
+print('&=' * 12)
+print('\n**** TABELA IMC ****')
+print('- Abaixo de 18.5: Abaixo do peso\n'
+      '- Entre 18.5 e 24.9: Peso ideal\n'
+      '- 25 até 39.9: Sobrepeso\n'
+      '- 30 até 40: Obesidade\n'
+      '- Acima de 40: Obesidade mórbida')
+print('**** TABELA IMC ****\n')
+altura = float(input('Qual a sua altura? '))
+peso = float(input('Quanto você pesa? '))
+imc = peso / (altura * altura)
+seuImc = imc
+print('\n#### RESULTADO ####')
+print(f'Seu IMC está em: {seuImc:.2f}.')
+if seuImc < 18.5:
+    print('Abaixo de 18.5: Abaixo do peso')
+elif seuImc >= 18.5 and seuImc <= 24.9:
+    print('- Entre 18.5 e 24.9: Peso ideal')
+elif seuImc >= 25 and seuImc <= 39.9:
+    print('- 25 até 39.9: Sobrepeso')
+elif seuImc >= 30 and seuImc <= 39.9:
+    print('- 30 até 39.9: Obesidade')
+else:
+    print('- Acima de 40: Obesidade mórbida')
--- python/mundo_02/PythonExercicios/ex044.py
+++ python/mundo_02/PythonExercicios/ex044.py
@@ -2,8 +2,45 @@
 Elabore um programa que calcule o valor a ser pago por um produto,
 considerando seu PREÇO NORMAL e CONDIÇÃO DE PAGAMENTO:
 
-- à vista dinheiro/ cheque: 10% de desconto
-- à vista no cartão: 5% de desconto
-- em até 2x no cartão: preço normal
-- 3x ou mais no cartão: 20% de juros
+1 - à vista dinheiro/ cheque: 10% de desconto
+2 - à vista no cartão: 5% de desconto
+3 - em até 2x no cartão: preço normal
+4 -  3x ou mais no cartão: 20% de juros
 """
+precoInformado = float(input('Informe o preço do produto R$ '))
+acrecimoJuros = (20 * precoInformado) / 100
+condicao1 = ((10 * precoInformado) / 100) + precoInformado
+condicao2 = ((5 * precoInformado) / 100) + precoInformado
+condicao3 = (precoInformado / 2) + precoInformado # CORRIGIR!! ESTÁ INFORMANDO TOTAL MAIOR DO QUE DEVERIA
+condicao4 = ((precoInformado / 3) + acrecimoJuros) + precoInformado
+# print(precoInformado)
+print('\n**** Condição de Pagamento ****\n'
+      '1 - à vista dinheiro/ cheque: 10% de desconto\n'
+      '2 - à vista no cartão: 5% de desconto\n'
+      '3 - em até 2x no cartão: preço normal\n'
+      '4 - 3x ou mais no cartão: 20% de juros\n')
+condicaoPagamento = int(input('Escolha a Condição do Pagamento: '))
+if condicaoPagamento == 1:
+    print(f'\nValor total de sua compra ficou em R$ {condicao1:.2f}')
+elif condicaoPagamento == 2:
+    print(f'\nValor total de sua compra ficou em R$ {condicao2:.2f}')
+elif condicaoPagamento == 3:
+    print(f'\nValor total de sua compra ficou em R$ {condicao3:.2f}')
+elif condicaoPagamento == 4:
+    print(f'\nValor total de sua compra ficou em R$ {condicao4:.2f}')
+else:
+    print(f'\nCondição de pagamento: {condicaoPagamento} inválida. '
+          f'\nEscolha uma das opções informadas.')
+
+
+
+
+
+
+
+
+
+# print(f'{condicao1:.2f}')
+# print(f'{condicao2:.2f}')
+# print(f'{condicao3:.2f}')
+#print(f'{condicao4:.2f}')
