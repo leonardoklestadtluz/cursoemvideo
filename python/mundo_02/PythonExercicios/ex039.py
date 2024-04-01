48437dc9387836f214b4bb2896d54a3df1d1f895
Leonardo Klestadt Luz <klestadt@hotmail.com>
2024-03-31 22:11:18 -0300

    Mundo 2: conclusão do exercício 39

--- python/mundo_02/PythonExercicios/ex039.py
+++ python/mundo_02/PythonExercicios/ex039.py
@@ -9,45 +9,23 @@ de um jovem e informe, de acordo com sua idade:
 
 Seu programa também deverá mostrar o tempo que falta ou que já passou do prazo
 """
+print('=====' * 5)
+print('** ALISTAMENTO MILITAR **')
+print('=====' * 5)
 anoAtual = date.today().year
-sexo = str(input('Informe seu sexo, M ou F? '))
-anoNascimento = int(input('Qual ano de nascimento? '))
-idadeAlistamento = 18
-idade = anoAtual - anoNascimento
-tempoFaltante = idadeAlistamento - idade
-tempoExcedido = idade - idadeAlistamento
-if idade < 18:
-    print(f'Você tem {idade} anos. Ainda faltam {tempoFaltante} anos para seu alistamento.')
-elif idade == 18:
-    print(f'Você tem {idade}, anos. Aliste-se IMEDIATAMENTE!')
-elif idade > 18:
-    print(f'[[ ATENÇÃO ]] Você tem {idade} anos. Já se passaram {tempoExcedido} anos de seu alistamento!\n'
-          f'Compareça a uma base militar, RÁPIDO!')
-
-# FALTA DESENVOLVER A CONDIÇÃO DO SEXO FEMININO
-
+sexo = str(input('\nInforme seu sexo, M ou F? '))
 if sexo == 'F' or sexo == 'f':
-    print(sexo)
+    print(f'\nO serviço militar não é obrigatório para mulheres.')
 else:
-    print(f'O serviço militar não é obrigatório para mulheres.')
-    opcao = input('Deseja alistar-se, S ou N? ')
-    if opcao == 'S' or opcao == 's':
-        print('Preencha o formulário com suas informações')
-    elif opcao == 'N' or opcao == 'n':
-        print('Obrigado por suas respostas.')
-
-
-
-
-
-
-
-
-
-
-# print(f'Ano atual: {anoAtual}')
-# print(f'Ano de nascimento: {anoNascimento}')
-# print(f'Sexo: {sexo}')
-
-
-
+    anoNascimento = int(input('Qual ano de nascimento? '))
+    idadeAlistamento = 18
+    idade = anoAtual - anoNascimento
+    tempoFaltante = idadeAlistamento - idade
+    tempoExcedido = idade - idadeAlistamento
+    if idade < 18:
+        print(f'Você tem {idade} anos. Ainda faltam {tempoFaltante} anos para seu alistamento.')
+    elif idade == 18:
+        print(f'Você tem {idade}, anos. Aliste-se IMEDIATAMENTE!')
+    elif idade > 18:
+        print(f'[[ ATENÇÃO ]] Você tem {idade} anos. Já se passaram {tempoExcedido} anos de seu alistamento!\n'
+              f'Compareça a uma base militar, RÁPIDO!')
