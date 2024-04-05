30f916fa23130111be2959d076ea42f8124d24e5
Leonardo Klestadt Luz <klestadt@hotmail.com>
2024-04-05 09:03:04 -0300

    Python 3 - Mundo 2: aula 13 - Estrutura de repetição for, em andamento tempo: 23:30

---
+++ python/mundo_02/pythonteste/aula13.py
@@ -0,0 +1,19 @@
+i = int(input('Início? '))
+f = int(input('Fim? '))
+p = int(input('Passo? '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
+
+
+
+"""
+for c in intervalo(início_intervalo, fim_intervalo):
+    print('Oi')
+print('FIM')
+
+
+for c in range(0, 6, 2):
+    print(c)
+print('FIM')
+"""
