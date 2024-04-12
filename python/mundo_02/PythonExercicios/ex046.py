3eeaa89db3651894379eb1e18e30de94f108e004
Leonardo Klestadt Luz <klestadt@hotmail.com>
2024-04-11 23:50:32 -0300

    Python 3 - Mundo 2: conclusão do exercício Contagem de pares

--- python/mundo_02/PythonExercicios/ex046.py
+++ python/mundo_02/PythonExercicios/ex046.py
@@ -1,4 +1,14 @@
+from time import sleep
+import emoji
 """
 Faça um programa que mostre na tela uma CONTAGEM REGRESSIVA para o estouro de fogos de
 artifício, indo de 10 ATÉ 0, com uma pausa de 1 SEGUNDO entre eles.
 """
+print(emoji.emojize(':candle:', language='alias') * 9)
+print('CONTAGEM REGRESSIVA')
+print(emoji.emojize(':candle:', language='alias') * 9)
+for c in range(10, 0, -1):
+    sleep(1)
+    print(emoji.emojize(f':ZZZ::ZZZ::ZZZ: {c}', language='alias'))
+sleep(1)
+print(emoji.emojize(':collision::collision::collision::collision::collision::collision:', language='alias'))
--- python/mundo_02/PythonExercicios/ex048.py
+++ python/mundo_02/PythonExercicios/ex048.py
@@ -1,4 +1,7 @@
 """
-Faça um programa que calcula a SOMA entre todos os NÚMEROS ÍMPARES  ue são MÚLTIPLOS DE TRÊS e que se encontram
+Faça um programa que calcula a SOMA entre todos os NÚMEROS ÍMPARES  que são MÚLTIPLOS DE TRÊS e que se encontram
 no intervalo de 1 até 500.
 """
+for numero in range(1, 500 + 1):
+  if numero % 2 != 0:
+    print(numero)
