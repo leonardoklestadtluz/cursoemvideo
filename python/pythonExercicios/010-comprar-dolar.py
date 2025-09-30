cd4fc432cc341aa5b55e4bf1396e96dea8997dcb
leonardoklestadtluz <klestadt@hotmail.com>
2025-09-29 22:50:47 -0300

    CURSO de python 3 - mundo 1: fundamentos, fazer exercícios da AULA Curso Python #07 - Operadores Aritméticos, conclusao do exercício 10

--- python/pythonExercicios/010-comprar-dolar.py
+++ python/pythonExercicios/010-comprar-dolar.py
@@ -1,4 +1,9 @@
-#010 Crie um programa que leia quanto dinheiro uma  pessoa tem na carteira e mostre quantos dólares ela pode comprar.
-
-# Considere
-# U$$ 1,00 = R$ 3,27
+#010 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+# Considere U$$ 1,00 = R$ 5,32
+print('=======================')
+print('==== COMPRAR DÓLAR ====')
+print('=======================\n')
+dinheiroCarteira = float(input('Quanto R$ tem na carteira? '))
+valorDolaR = 5.32
+quantidadeDolar = dinheiroCarteira / valorDolaR
+print(f'Com R$ {dinheiroCarteira:.2f} você compra US$ {quantidadeDolar:.2f}.')
--- python/pythonExercicios/011-qtd-tinta-pintar-parede.py
+++ python/pythonExercicios/011-qtd-tinta-pintar-parede.py
@@ -1,2 +1,3 @@
-#011 Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².
+#011 Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade de
+# tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².
 
