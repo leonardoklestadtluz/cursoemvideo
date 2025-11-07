98b84a61d21c3c5a009f07023e9b129062aad033
leonardoklestadtluz <klestadt@hotmail.com>
2025-11-06 22:16:04 -0300

    CURSO de python 3 - mundo 1: fundamentos - exercíco python #015,edição do código-fonte

--- python/pythonExercicios/015-aluguel-veiculo.py
+++ python/pythonExercicios/015-aluguel-veiculo.py
@@ -5,9 +5,9 @@
 Calcule o preço a pagar, sabendo que o carro custa R$60/ dia e R$0,15/ km rodado.
 
 """
-print('========================')
-print('==== CALC ALUGUEL ======')
-print('========================\n')
+print('====================================')
+print('==== CALC ALUGUEL DE VEÍCULOS ======')
+print('====================================\n')
 quilometroPercorrido = float(input('Km percorrido? '))
 diasAlugado = int(input('Dias alugado? '))
 valorDiaria = float(60.00)
@@ -15,7 +15,7 @@ valorKmRodado = float(0.15)
 valorTotalDiaria = diasAlugado * valorDiaria
 valorTotalKm = quilometroPercorrido * valorKmRodado
 totalAPagar = valorTotalDiaria + valorTotalKm
-print('$$$$$$$$$$$$$$$$$$$$$$$')
+print('\n$$$$$$$$$$$$$$$$$$$$$$$')
 print(f'TOTAL A PAGAR R$ {totalAPagar:.2f}')
 print('$$$$$$$$$$$$$$$$$$$$$$$\n')
 print(f'Km percorrido: {quilometroPercorrido:.2f} quilômetros.\n')
