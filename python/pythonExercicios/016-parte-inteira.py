0be25d8410bdfd94ed12af1bd0dfff2ccb7c25c9
leonardoklestadtluz <klestadt@hotmail.com>
2025-11-07 21:25:18 -0300

    CURSO de python 3 - mundo 1: fundamentos - curso python #08 - utilizando módulos

---
+++ python/pythonExercicios/016-parte-inteira.py
@@ -0,0 +1,5 @@
+"""
+
+016 Crie um programa quye leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+
+"""
---
+++ python/pythonExercicios/021-arquivo-mp3.py
@@ -0,0 +1,5 @@
+"""
+
+021 Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+"""
