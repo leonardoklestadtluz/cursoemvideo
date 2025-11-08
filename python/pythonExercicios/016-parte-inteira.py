be904839bdc996882be3792738e78bb81fda6866
leonardoklestadtluz <klestadt@hotmail.com>
2025-11-07 22:41:00 -0300

    CURSO de python 3 - mundo 1: fundamentos - conclusão do exercício python #16 - quebrando um número

--- python/pythonExercicios/016-parte-inteira.py
+++ python/pythonExercicios/016-parte-inteira.py
@@ -1,5 +1,13 @@
 """
 
-016 Crie um programa quye leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+016 Crie um programa que leia um número real qualquer pelo teclado
+ e mostre na tela a sua porção inteira.
 
 """
+
+print('-----------------------')
+print('EXIBIDOR PORÇÃO INTEIRA')
+print('-----------------------\n')
+numero = float(input('Digite um número real qualquer: '))
+print(f'Parte inteira: {int(numero)}\n'
+      f'Número real digitado: {numero}')
--- python/pythonExercicios/017-comprimento-hipotenusa.py
+++ python/pythonExercicios/017-comprimento-hipotenusa.py
@@ -3,4 +3,16 @@
  cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
 
 """
+from math import sqrt, pow
+print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+print('^^ COMPRIMENTO DA HIPOTENUSA ^^')
+print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+catetoOposto = int(input('Quanto mede o cateto oposto? '))
+catetoAdjacente = int(input('Qual a medida do cateto adjacente? '))
+comprimentoCatetos = (catetoOposto ** 2) + (catetoAdjacente ** 2)
+hipotenusa = sqrt(comprimentoCatetos)
+print(f'Comprimento da hipotenusa = {hipotenusa:.3f}\n'
+      f'Cateto oposto = {catetoOposto:.3f}\n'
+      f'Cateto adjacente = {catetoAdjacente:.3f}'
+)
 
