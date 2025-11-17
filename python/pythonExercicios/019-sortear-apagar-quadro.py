b0193b76a4a960f18df70d34bbb98e3ad5cebe9d
leonardoklestadtluz <klestadt@hotmail.com>
2025-11-16 23:28:49 -0300

    CURSO de python 3 - mundo 1: fundamentos - conclusão do exercício python #019 - sorteando um item na lista

--- python/pythonExercicios/019-sortear-apagar-quadro.py
+++ python/pythonExercicios/019-sortear-apagar-quadro.py
@@ -4,3 +4,12 @@
 Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
 
 """
+import random
+aluno1 = str(input('Nome do 1º aluno? '))
+aluno2 = str(input('Nome do 2º aluno? '))
+aluno3 = str(input('Nome do 3º aluno? '))
+aluno4 = str(input('Nome do 4º aluno? '))
+lista = [aluno1, aluno2, aluno3, aluno4]
+sorteado = random.choices(lista)
+print(f'Aluno {sorteado[0]}, foi o sorteado: ')
+
