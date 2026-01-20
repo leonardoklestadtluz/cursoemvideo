9231e991b04fabee1796a330e8376ff829e09718
leonardoklestadtluz <klestadt@hotmail.com>
2026-01-20 08:48:50 -0300

    CURSO de python 3 MUNDO 2, renomeando arquivos da pasta PythonExercicios

--- python/mundo_01/pythonteste/aula11.py
+++ python/mundo_01/pythonteste/aula11.py
@@ -1,12 +1,22 @@
+#print('Hello World')
+#print('\033[1;31;43mHello, world!')
+#print('\033[1;31;43mHello, world!\033[m')
+#print('\033[4;30;45mHello, wolrd!\033')
+#print('\033[7;30mOlá, mundo!\033[m')
+#print('\033[7;33;44mOlá, mundo!\033[m')
+
+#a = 3
+#b = 5
+#print(f'Os valores são \033[32m{a}\033[m e \033[31m{b}\033[m!!!')
+
 nome = 'Leonardo'
 cores = {
-    'limpa': '\033[m',
-    'azul': '\033[34m',
-    'amarelo': '\033[33m',
-    'pretoEbranco': '\033[7;38m'
+    'limpa':'\033[m',
+    'azul':'\033[34m',
+    'amarelo':'\033[33m',
+    'pretoBranco':'\033[7:30m'
 }
-print(f'Olá! Muito prazerem te conehcer, meu nome é {cores['pretoEbranco']}{nome}{cores['limpa']}.')
-# a = 3
-# b = 5
-# print(f'Os valores são \033[32m{a}\033[m e \033[31m{b}\033[m.')
-# print('\033[7;33;44mHello, world!\033[m')
+print(f'Olá! Muito prazer em te conhecer, {cores["amarelo"]}{nome}{cores["limpa"]}!!!')
+
+
+
