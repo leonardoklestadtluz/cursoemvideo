397e2d647da384039f188b6815688702a7b0e8ce
leonardoklestadtluz <klestadt@hotmail.com>
2026-01-28 23:13:56 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python (for), AULA exercício 52 - números primos, concluída.

--- python/mundo_02/PythonExercicios/052-numeros-primos.py
+++ python/mundo_02/PythonExercicios/052-numeros-primos.py
@@ -1,3 +1,20 @@
 """
 Faça um programa que leia um NÚMERO INTEIRO e diga se ele é ou não um NÚMERO PRIMO.
 """
+numero = int(input('Digite um número: '))
+tot = 0
+for c in range(1, numero + 1):
+    if numero % c == 0:
+        print(f'\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print(f'{c}', end=', ')
+print(f'\n\033[mO número {numero} foi divisível {tot} vez(es).')
+if tot == 2:
+    print('E por isso ele é PRIMO')
+else:
+    print('E por isso ele NÃO é primo')
+
+
+
