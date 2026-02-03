ffd14f2ad04136f1e7c3d27302f2bc7bdd9d73bc
leonardoklestadtluz <klestadt@hotmail.com>
2026-02-02 23:11:07 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python (for), AULA exercício 53 - detector de palíndromo, concluída.

--- python/mundo_02/PythonExercicios/053-detector-de-palindromo.py
+++ python/mundo_02/PythonExercicios/053-detector-de-palindromo.py
@@ -1,3 +1,24 @@
 """
-Crie um programa que leia uma FRASE qualquer e diga se ela é um PALÍDROMO, desconsiderando os espaços.
+Crie um programa que leia uma FRASE qualquer e diga se ela é um PALÍNDROMO, desconsiderando os espaços.
 """
+
+# radar
+# o rato roeu a roupa do rei de roma
+
+print('*' * 22)
+print('DETECTOR DE PALÍNDROMO')
+print('*' * 22)
+frasePalavra = str(input('Escreva uma frase ou palavra(s): ')).strip().upper()
+palavras = frasePalavra.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+'''for letra in range(len(junto) - 1, - 1, -1):
+    inverso += junto[letra]'''
+print(f'O inverso de {junto} é {inverso} ')
+if inverso == junto:
+    print(f'A frase/ palavra {frasePalavra} é um PALÍNDROMO')
+else:
+    print(f'A frase/ palavra {frasePalavra} não é um PALÍNDROMO')
+
+
+
