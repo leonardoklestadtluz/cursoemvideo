21d6e2108619f9d450de357bf0e944d48bfc497d
leonardoklestadtluz <klestadt@hotmail.com>
2026-02-03 23:58:57 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python (for), AULA exercício 54 - grupo da maioridade, concluída.

--- python/mundo_02/PythonExercicios/054-grupo-da-maioridade.py
+++ python/mundo_02/PythonExercicios/054-grupo-da-maioridade.py
@@ -2,3 +2,16 @@
 Crie um programa que leia o ANO DE NASCIMENTO de SETE PESSOAS.
 No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
 """
+from  datetime import date
+anoAtual = date.today().year
+totalMaior = 0
+totalMenor = 0
+for pessoa in range(1, 8):
+    anoNascimento = int(input(f'Em que ano a {pessoa}ª pessoa nasceu? '))
+    idade = anoAtual - anoNascimento
+    if idade >= 21:
+        totalMaior += 1
+    else:
+        totalMenor += 1
+print(f'Ao todo tivemos {totalMaior} pessoas maiores de idade.')
+print(f'E também tivemos {totalMenor} pessoas menores de idade.')
