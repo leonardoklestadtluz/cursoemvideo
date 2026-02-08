c97fc86efb88db5aaf7700c7608bfb90a0a776db
leonardoklestadtluz <klestadt@hotmail.com>
2026-02-08 00:01:29 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python(for), concluído.

--- python/mundo_02/PythonExercicios/055-maior-e-menor-da-sequencia.py
+++ python/mundo_02/PythonExercicios/055-maior-e-menor-da-sequencia.py
@@ -2,3 +2,17 @@
 Faça um programa que leia o PESO de CINCO PESSOAS.
 No final, mostre qual foi o MAIOR e o MENOR peso lidos.
 """
+pesoMaior = 0
+pesoMenor = 0
+for pessoa in range(1, 6):
+    peso = float(input(f'Digite o peso da {pessoa}ª pessoa: '))
+    if pessoa == 1:
+        pesoMaior = peso
+        pesoMenor = peso
+    else:
+        if peso > pesoMaior:
+            pesoMaior = peso
+        if peso < pesoMenor:
+            pesoMenor = peso
+print(f'O maior peso lido foi de {pesoMaior}Kg.')
+print(f'O menor peso lido foi de {pesoMenor}Kg.')
--- python/mundo_02/PythonExercicios/056-analisador-completo.py
+++ python/mundo_02/PythonExercicios/056-analisador-completo.py
@@ -7,3 +7,34 @@ No final do programa, mostre:
     * Qual é o nome do homem MAIS VELHO.
     * Quantas mulheres tem MENOS DE 20 anos.
 """
+somaIdade = 0
+mediaIdade = 0
+maiorIdadeHomem = 0
+nomeMaisVelho = ''
+totMulher20 = 0
+for pessoa in range(1, 5):
+    print(f'\n---- {pessoa}ª PESSOA ----')
+    nome = str(input(f'Qual é o nome {pessoa}ª pessoa? '))
+    idade = int(input(f'Qual a idade da {pessoa}ª pessoa? '))
+    sexo = str(input(f'Qual o seu sexo da {pessoa}ª pessoa [M/F]? ')).strip()
+    somaIdade += idade
+    if pessoa == 1 and sexo in 'Mm':
+        maiorIdadeHomem = idade
+        nomeMaisVelho = nome
+    if sexo in 'Mm' and idade > maiorIdadeHomem:
+        maiorIdadeHomem = idade
+        nomeMaisVelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totMulher20 += 1
+mediaIdade = somaIdade / 4
+print(f'\nA média de idade do grupo é de {mediaIdade} anos.')
+print(f'O homem mais velho tem {maiorIdadeHomem} anos e se chama {nomeMaisVelho}.')
+print(f'Ao todo são {totMulher20} mulheres com menos de 20 anos.')
+
+
+
+
+
+
+
+
