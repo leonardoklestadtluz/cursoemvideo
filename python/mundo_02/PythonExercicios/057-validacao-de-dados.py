6d61da569fdec528631fabcafe4bdd4940c870f6
leonardoklestadtluz <klestadt@hotmail.com>
2026-03-07 22:46:14 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python (while), EXERCÍCIO 57 - validação de dados, concluída.

--- python/mundo_02/PythonExercicios/057-validacao-de-dados.py
+++ python/mundo_02/PythonExercicios/057-validacao-de-dados.py
@@ -2,3 +2,13 @@
 Faça um programa que leia o sexo de uma pessoa, mas só aceita os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
 """
+print('='*20)
+print(' VALIDADOR DE DADOS ')
+print('='*20)
+sexo = str(input('Informe um sexo [M/F]: ')).strip().upper()
+while sexo != 'M' and sexo != 'F':
+    if sexo == 'M':
+        sexo
+    else:
+        sexo = str(input('Informe um sexo [M/F]: ')).strip().upper()
+print(f'Sexo digitado: {sexo}')
