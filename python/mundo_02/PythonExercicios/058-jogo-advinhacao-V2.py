f5440ee4c8be78ba6487fa0850dfec3b410eafa7
leonardoklestadtluz <klestadt@hotmail.com>
2026-03-15 16:46:13 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python (while), EXERCÍCIO 58 - jogo da adivinhação V 2, concluída.

--- python/mundo_02/PythonExercicios/058-jogo-advinhacao-V2.py
+++ python/mundo_02/PythonExercicios/058-jogo-advinhacao-V2.py
@@ -2,3 +2,22 @@
 Melhore o jogo do DESAFIO 028 onde o computador vai 'pensar' em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram nececessários para vencer.
 
 """
+from random import randint
+computador = randint(0,10)
+print('Sou seu computado... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi?')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        else:
+            print('Menos. Tente mais uma vez.')
+print(f'ACERTOU com {palpites} tentativas!')
+
+
