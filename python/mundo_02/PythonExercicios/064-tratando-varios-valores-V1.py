39c6a647c9af97c08d480152bd26a5ccf7381f12
leonardoklestadtluz <klestadt@hotmail.com>
2026-03-26 21:06:29 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python (while), EXERCÍCIO 65 - maior e menor valores, concluída.

--- python/mundo_02/PythonExercicios/064-tratando-varios-valores-V1.py
+++ python/mundo_02/PythonExercicios/064-tratando-varios-valores-V1.py
@@ -4,3 +4,10 @@ O programa só vai parar quando o usuário digitar o valor 999, que é a condiç
 No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag 999)
 
 """
+num = cont = soma = 0
+num = int(input('Digite um número [999 pra parar] >> '))
+while num != 999:
+    soma += num
+    cont += 1
+    num = int(input('Digite um número [999 pra parar] >> '))
+print(f'Você digitou {cont} números. E a soma entre eles é {soma}.')
--- python/mundo_02/PythonExercicios/065-maior-e-menor-valores.py
+++ python/mundo_02/PythonExercicios/065-maior-e-menor-valores.py
@@ -1,4 +1,27 @@
 """
-Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar a o usuário se ele quer ou não continuar a digitar valores.
+Crie um programa que leia vários números inteiros pelo teclado.
+
+No final da execução, mostre a média entre todos os valores
+e qual foi o maior e o menor valores lidos.
+
+O programa deve perguntar ao usuário se ele quer
+ou não continuar a digitar valores.
 
 """
+resp = 'S'
+soma = qtd = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    qtd += 1
+    if qtd == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / qtd
+print(f'Você digitou {qtd} números e a média foi {media}.')
+print(f'O maior número digitado foi {maior} e o menor número digitado foi {menor}.')
