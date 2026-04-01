c2f9eb4abe87abfd60fa6d9d7ea8112d44a24a43
leonardoklestadtluz <klestadt@hotmail.com>
2026-03-31 23:24:04 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python (while), AULA exercício 66 - vários números com flag, concluída.

--- python/mundo_02/PythonExercicios/066-varios-numeros-com-flag.py
+++ python/mundo_02/PythonExercicios/066-varios-numeros-com-flag.py
@@ -1,5 +1,20 @@
 """
 
-Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
+Crie um programa que leia vários números inteiros pelo teclado.
+O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
+No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
 
 """
+n = c = s = 0
+while True:
+    n = int(input('Digite um número inteiro qualquer (999 pra parar): '))
+    if n <= 0:
+        print(f'[ERRO]: Número {n} é inválido. Fim do programa')
+        break
+    if n == 999:
+        break
+    s += n
+    c += 1
+print(f'\nTotal de números digitados: {c}')
+print(f'A soma dos números digitados foi: {s}')
+
