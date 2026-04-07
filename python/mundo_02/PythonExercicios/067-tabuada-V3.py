503b2eaa65a1476ea37973df7488dc4b6b7c86f1
leonardoklestadtluz <klestadt@hotmail.com>
2026-04-07 18:04:54 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python (while), AULA exercício 68 - jogo do par ou ímpar, concluída.

--- python/mundo_02/PythonExercicios/067-tabuada-V3.py
+++ python/mundo_02/PythonExercicios/067-tabuada-V3.py
@@ -4,3 +4,18 @@ Faça um programa que mostre a tabuada de vários números, um de cada vez, para
 O programa será interrompido quando o número solicitado for negativo.
 
 """
+print('*' * 15)
+print('*** TABUADA ***')
+print('Digite qualquer valor negativo para encerrar.')
+print('*' * 15)
+while True:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    if n < 0:
+        print('=' * 20)
+        break
+    else:
+        print('-' * 20)
+        for c in range(1, 11):
+            print(f'{n} x {c} = {n * c}')
+        print('-' * 20)
+print('PROGRAMA DE TABUADA ENCERRADO. VOLTE SEMPRE!')
--- python/mundo_02/PythonExercicios/068-jogo-do-par-ou-impar.py
+++ python/mundo_02/PythonExercicios/068-jogo-do-par-ou-impar.py
@@ -1,5 +1,42 @@
 """
 
-Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder,
+mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
 
 """
+print('=-' * 13)
+print('VAMOS JOGAR PAR OU ÍMPAR')
+print('=-' * 13)
+from random import randint
+v = 0
+while True:
+    jogador = int(input('Que número você quer? '))
+    computador = randint(0, 10)
+    total = jogador + computador
+    escolha = ' '
+    while escolha not in 'PI':
+        escolha = str(input('PAR ou ÍMPAR? [P/I] ')).upper().strip()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total}, ', end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR')
+    if escolha == 'P':
+        if total % 2 == 0:
+            print('Você VENCEU!!')
+            v += 1
+        else:
+            print('Você PERDEU')
+            break
+    elif escolha == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!!')
+            v += 1
+        else:
+            print('Você PERDEU!!')
+            break
+    print('Vamor jogar novamente...')
+print(f'GAME OVER!! Você venceu {v} vezes.')
+
+
+
+
+
+
