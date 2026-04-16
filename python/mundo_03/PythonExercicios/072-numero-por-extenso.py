2bcb090d3c5e387062a10b81278e6f55d11f3508
leonardoklestadtluz <klestadt@hotmail.com>
2026-04-16 18:57:36 -0300

    CURSO de python 3, MUNDO 2, MÓDULO repetições em python (while), AULA exercicio 72 - número por extenso, concluída.

--- python/mundo_03/PythonExercicios/072-numero-por-extenso.py
+++ python/mundo_03/PythonExercicios/072-numero-por-extenso.py
@@ -2,3 +2,19 @@
 Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
 Seu programa deverá ler o número pelo teclado(entre 0 e 20) e transformá-lo por extenso
 """
+cont = ('zero','um','dois','tres','quatro',
+        'cinco','seis','sete','oito',
+        'nove','dez','onze','doze',
+        'treze','quatorze','quinze','dezesseis',
+        'dezessete','dezoito','dezenove','vinte')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('[ERRO] Tente Novamente! ', end='')
+print(f'Você digitou o número {cont[num]}.')
+
+
+
+
+
